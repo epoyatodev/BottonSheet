b1aa542d481b8917ea71133d4c509f9b2899d05d
EnriqueDev <43639512+epoyatodev@users.noreply.github.com>
2023-06-25 12:27:56 +0200

    Initial Commit

---
+++ BottonSheet/BottonSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottonSheetApp.swift
+//  BottonSheet
+//
+//  Created by Enrique Poyato Ortiz on 25/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BottonSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
